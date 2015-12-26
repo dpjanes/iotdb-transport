53aaa9197ea9d8dfcb2201b1e430fc442ce596f7
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:49 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -36,10 +36,9 @@ echo "=================="
         index.js connect.js transporter.js \
         bin/transport \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
