254c24f93d8ae909ae3cf9d14fc07e724ee04c11
David Janes <davidjanes@davidjanes.com>
2016-02-03 12:24:59 -0500

    remove old bunyan calls

--- DIST.sh
+++ DIST.sh
@@ -33,7 +33,7 @@ echo "=================="
         --exclude "node_modules" \
         README.md LICENSE \
         homestar.json package.json \
-        index.js connect.js transporter.js \
+        index.js connect.js transporter.js Transport.js helpers.js errors.js \
         bin/transport \
         |
     ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
